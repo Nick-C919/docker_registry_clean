b2a97c48312386272cea5c9a4918df98caa5adc0
Kuhn <170478063@qq.com>
2015-11-10 14:56:36 +0800

    新增删除某image无用层

--- delete_image.py
+++ delete_image.py
@@ -6,7 +6,7 @@
 
 import os,sys,shutil
 import json
-
+import glob
 
 #将仓库中所有节点关系导出
 class Nodes:
@@ -39,10 +39,11 @@ class Nodes:
 #元数据处理
 class Metadata:
     # 初始化
-    def __init__(self, registry_path,image_name,image_tag):
+    def __init__(self, registry_path,image_name="",image_tag="",nodes=[]):
         self.registry_path = registry_path
         self.image_name  = image_name
         self.image_tag  = image_tag
+        self.nodes  = nodes
     #获取目标image_name：image_name的ID
     def get_image_id(self):
         try:
@@ -80,23 +81,57 @@ class Metadata:
         image_index_f.write(image_index_new)
         image_index_f.close()
 
-#删除某个镜像
+    #删除所有镜像index，得出无用index列表
+    def futility_meta_index(self,image_id,index_list):
+        # print self.nodes[1].count(image_id)
+        if image_id in self.nodes[0]:
+            try:
+                index_list.remove({'id':image_id})
+            except Exception,e:
+                print e
+            parent_id = self.nodes[0][image_id]
+            # print parent_id
+            # print index_list
+            index_list = self.futility_meta_index(parent_id,index_list)
+            return index_list
+        else:
+            # print "hahah"
+            # print index_list
+            return index_list
+
+    #将某image下，所有tag排除后，得到已无用的镜像层列表，return出去，待用
+    def futility_meta(self):
+        #遍历image下所有tag
+        os.chdir('.')
+        image_index_file = self.registry_path + "/repositories/library/" + self.image_name + "/_index_images"
+        image_index_f = open( image_index_file  , "r" )
+        image_index =  image_index_f.readline()
+        image_index_f.close()
+        index_list = json.loads(image_index)
+        # index_list_new = index_list
+        for file in glob.glob(self.registry_path + "/repositories/library/" + self.image_name +'/tag_*'):
+            print file
+            self.image_tag = file.split('_')[-1]
+            image_id = self.get_image_id()
+            index_list = self.futility_meta_index(image_id,index_list)
+        return index_list
+
+
+#删除某个镜像:tag
 class Delete_image:
     # 初始化
-    def __init__(self, registry_path,image_name,image_tag,nodes):
+    def __init__(self, registry_path ,image_name , nodes ,image_tag = ""):
         self.registry_path = registry_path
         self.image_name  = image_name
         self.image_tag  = image_tag
         self.nodes  = nodes
         # print self.nodes
 
-    #打印镜像递归列表：
-    def print_image_list(self):
-        print "print_image_list"
-
     #删除镜像实体
+    #这里有个重大问题，如果该镜像依赖的基础镜像存在，则如果tag只有一个，会递归把基础镜像删除，而却校验不出来，因为依赖都是1
+    #初步想法是，笨办法就是将所有镜像过滤一遍，获取所有Image:tag的最后层ID，做标记
     def delete_image(self,image_id_t,metadata,begin='Y'):
-        print "[INFO]   ID：" + str(image_id_t)
+        print "[INFO]   ID：" + str(image_id_t)+str(self.nodes[1].count(image_id_t))
         if (self.nodes[1].count(image_id_t) == 0 and begin=='Y') or ( self.nodes[1].count(image_id_t) == 1 and begin=='N' ) :#判断是否被依赖
             print "[INFO]   删除元数据index信息"
             metadata.delete_index(str(image_id_t))
@@ -117,6 +152,25 @@ class Delete_image:
                 print str(image_id_t)+"不存在，不需要删除"
         else:
             print "[INFO]   ID：" + str(image_id_t) + "被依赖"
+
+    #删除镜像实体
+    #这里需要在前期制作镜像的时候做严格的分层，即系统镜像、基础镜像、业务镜像，因为如果不严格，可能出现严重的删除后丢失情况
+    #异常场景：如果某镜像基于某基础镜像，做了一个tag，但是该tag的第二个版本没有使用严格的原基础镜像，而是新建了一个毫无关联的镜像，基础镜像也会被认为是无用镜像，会被删除掉！！！
+    def clean_image(self,image_id_t,metadata):
+        print "[INFO]   ID：" + str(image_id_t)+str(self.nodes[1].count(image_id_t))
+        print "[INFO]   删除元数据index信息"
+        metadata.delete_index(str(image_id_t))
+        if str(image_id_t) in self.nodes[0]:#判断镜像是否能找到实体
+            print "[INFO]   删除image实体"
+            # os.remove(self.registry_path + "/images/" + image_id_t)
+            shutil.rmtree(self.registry_path + "/images/" + str(image_id_t))
+            #更新self.nodes
+            del self.nodes[0][image_id_t]
+            if image_id_t in self.nodes[1]:
+                self.nodes[1].remove(image_id_t)
+        else:
+            print str(image_id_t)+"不存在，不需要删除"
+
 
     #删除某个镜像，以及递归的父镜像，如果父镜像有其他依赖，则退出
     def run(self):
@@ -143,12 +197,21 @@ class Delete_image:
                 # print image_index_t['id']
                 self.delete_image(image_index_t['id'],metadata)
             image_index_f.close()
-
-        # image_index_files = os.listdir( self.registry_path + "/repositories/library/" + self.image_name )
-        # if len(image_index_files) == 1:
-        #     print "[INFO]   镜像"+self.image_name+"已清空，删除对应元数据信息"
-        #     shutil.rmtree(self.registry_path + "/repositories/library/" + self.image_name)
         print "[END]   删除镜像"+self.image_name+"完成"
+
+    #清理仓库，将某image下无用的层清理掉
+    def clean_run(self):
+        #打印镜像递归列表
+        # self.print_image_list()
+        #根据元数据获取image_ID
+        metadata = Metadata(self.registry_path,image_name=self.image_name,nodes=self.nodes)
+        futility_list = metadata.futility_meta()
+        print "[INFO]   元数据列表"
+        print futility_list
+        for image_id in futility_list:
+            #根据树结构删除Image节点以及对应index信息
+            self.clean_image(image_id['id'],metadata)
+        print "[END]   清理镜像"+self.image_name+"完成"
 
 # def usage():
 #     print "Usage:"
--- docker_registry_clean.py
+++ docker_registry_clean.py
@@ -4,11 +4,12 @@
 # company = 'Asiainfo'
 # qq:170478063
 
-import os,sys,shutil
+import os,sys,shutil,re
 import json
 from delete_image import Nodes
 from delete_image import Metadata
 from delete_image import Delete_image
+
 
 
 
@@ -18,19 +19,25 @@ def usage():
     print "    python delete-image.py  {registry_path}  {image_name:image_tag}"
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 3 :
         registry_path = sys.argv[1]
-        image_name =  sys.argv[2].split(':')[0]
-        image_tag =  sys.argv[2].split(':')[1]
+        if re.search(':',sys.argv[2]) == None:    #清理指定镜像内的无用层
+            image_name =  sys.argv[2].split(':')[0]
+            nodes_J = Nodes(registry_path)
+            nodes = nodes_J.nodes()
+            #删除指定的镜像tag
+            delete_image = Delete_image(registry_path,image_name,nodes)
+            delete_image.clean_run()
+        else:                                      #删除指定镜像指定tag
+            image_name =  sys.argv[2].split(':')[0]
+            image_tag =  sys.argv[2].split(':')[1]
+            nodes_J = Nodes(registry_path)
+            nodes = nodes_J.nodes()
+            #删除指定的镜像tag
+            delete_image = Delete_image(registry_path,image_name,nodes,image_tag)
+            delete_image.run()
     else:
         usage()
         sys.exit(0)
-    nodes_J = Nodes(registry_path)
-    nodes = nodes_J.nodes()
-    # print nodes[0]
-    # print nodes[1]
-    #删除指定的镜像tag
-    delete_image = Delete_image(registry_path,image_name,image_tag,nodes)
-    delete_image.run()
 
 
